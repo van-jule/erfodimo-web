d0d0a2a6b787bbefb167e19e8ba2a3d329f1f010
van-jule <jkharchenko2008@gmail.com>
2024-12-22 21:11:51 +0100

    first quote

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,31 @@
 import React from "react";
 
 export default function Page() {
-    return <h1>Hello, Next.js!</h1>
-  }
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        flexDirection: "column",
+        minHeight: "90vh",
+        fontFamily: "Georgia",
+      }}
+    >
+      <h1 style={{ marginBottom: "20px" }}>Max Erfodimo</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          flexDirection: "column",
+          fontSize: "24px",
+          gap: "10px",
+        }}
+      >
+        <div>Stay focused...</div>
+        <div> the result will come</div>
+      </div>
+    </div>
+  );
+}
